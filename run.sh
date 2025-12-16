2f4350a79aa67317812017cc49c1828ed1bff3f4
Ibrahim Onuralp Yigit <onuralpyigit@gmail.com>
2025-12-16 21:11:46 +0300

    Add validation step into run script.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Step 1: Generate
-python generate_graph.py --scale medium --scenario smart_city --output input/dataset.json
+python generate_graph.py --scale small --scenario iot --output input/dataset.json
 
 # Step 2: Import
 python import_graph.py --input input/dataset.json --clear
@@ -12,5 +12,8 @@ python analyze_graph.py --neo4j --output-dir output/ --format json html csv
 # Step 4: Simulate
 python simulate_graph.py --neo4j --exhaustive --output-dir output/ --format json html csv
 
-# Step 5: Visualize
+# Step 5: Validate
+python validate_graph.py --neo4j --output-dir output/ --format json html csv
+
+# Step 6: Visualize
 python visualize_graph.py --neo4j --output-dir output/ --full
